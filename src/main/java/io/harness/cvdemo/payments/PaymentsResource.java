73f7053e4bf90d6a76c75d251050ddd8170bbb55
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-10-07 22:59:42 -0300

    response time fix

--- src/main/java/io/harness/cvdemo/payments/PaymentsResource.java
+++ src/main/java/io/harness/cvdemo/payments/PaymentsResource.java
@@ -37,7 +37,7 @@ public class PaymentsResource {
     @GET
     @Path("list")
     public Response executeNormalCall() {
-        int max = 400, min = 200;
+        int max = 3400, min = 2200;
         int msDelay = r.nextInt((max - min) + 1) + min;
         try {
             Thread.sleep(msDelay);
