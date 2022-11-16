8d155b1e7142cf9bf2c91a8f6b0113989e9fc1a4
Diego Pereira <22509951+diegopereiraeng@users.noreply.github.com>
2022-11-16 13:48:24 -0300

    payment unit tests

--- src/test/java/io/harness/cvdemo/payments/PaymentsResourceTest.java
+++ src/test/java/io/harness/cvdemo/payments/PaymentsResourceTest.java
@@ -27,7 +27,6 @@ public class PaymentsResourceTest {
         assertEquals("Health Check OK","Health Check OK");
     }
 
-
     @Test
     public void paymentStatus() {
         System.out.println("Unit Test metricGeneratorHealthCheck");
